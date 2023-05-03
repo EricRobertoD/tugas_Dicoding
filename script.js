8d8ac8fe81f864cfff665fd5cf82172ed9570b94
Lepidophylla <ericroberto2322@gmail.com>
2023-05-04 00:25:29 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("daftarHobijsDropdown").onmouseenter = function(){
+    document.getElementsByClassName("daftar-hobi-dropdown")[0].style.display = "block";
+}
+
+document.getElementById("daftarHobijsDropdown").onmouseleave = function(){
+    document.getElementsByClassName("daftar-hobi-dropdown")[0].style.display = "none";
+}
